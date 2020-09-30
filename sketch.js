e9aa7bbb22b7cb8b2120450ac7749de691a13b4c
UniversalOverlord123 <68557624+UniversalOverlord123@users.noreply.github.com>
2020-09-30 17:22:31 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -23,45 +23,6 @@ function draw() {
   minAngle = map(minutehand,0,60,0,360);
   secAngle = map(secondhand,0,60,0,360);
 
-  push();
-  translate(400,400);
-  rotate(secAngle);
-  stroke("red");
-  strokeWeight(9);
-  line(0,0,100,0);
-  pop();
-
-  push();
-  translate(400,400);
-  rotate(minAngle);
-  stroke("yellow");
-  strokeWeight(6);
-  line(0,0,100,0);
-  pop();
-
-  push();
-  translate(400,400);
-  rotate(hrAngle);
-  stroke("green");
-  strokeWeight(3);
-  line(0,0,100,0);
-  pop();
-
-  push();
-  noFill();
-  stroke("red");
-  strokeWeight(15);
-  arc(400,400,260,260,-90,secAngle);
-
-  stroke("yellow");
-  strokeWeight(15);
-  arc(400,400,320,320,-90,minAngle);
-
-  stroke("green");
-  strokeWeight(15);
-  arc(400,400,380,380,-90,hrAngle);
-  pop();
-
   textAlign(CENTER);
   textSize(24);
    
@@ -99,4 +60,43 @@ function draw() {
   else{
     text("PM",475,150);
   }
+
+  translate(400,400);
+  rotate(-90);
+
+  push();
+  rotate(secAngle);
+  stroke("red");
+  strokeWeight(9);
+  line(0,0,100,0);
+  pop();
+
+  push();
+  rotate(minAngle);
+  stroke("yellow");
+  strokeWeight(6);
+  line(0,0,100,0);
+  pop();
+
+  push();
+  rotate(hrAngle);
+  stroke("green");
+  strokeWeight(3);
+  line(0,0,100,0);
+  pop();
+
+  push();
+  noFill();
+  stroke("red");
+  strokeWeight(15);
+  arc(0,0,260,260,0,secAngle);
+
+  stroke("yellow");
+  strokeWeight(15);
+  arc(0,0,320,320,0,minAngle);
+
+  stroke("green");
+  strokeWeight(15);
+  arc(0,0,380,380,0,hrAngle);
+  pop();
 }
